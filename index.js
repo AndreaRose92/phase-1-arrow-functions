54c54c3e9683e679bccc6e15230590c27ae9b7e5
Andrea Rosentel <andrearosentel@gmail.com>
2022-06-29 07:03:43 -0700

    commit tree 9ad0a19aae015481d53f829f2e6624240a1a7cde

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const divide = function(num1, num2) {
     return num1 / num2
 }
 
-const square = (num3) => num3 * num3
+const square = (num3) => num3 ** 2
 
 const add = (num4, num5) => num4 + num5
